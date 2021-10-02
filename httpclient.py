bd14cf794304f456f2df3faaf214e39b184b4b58
flyrobot27 <stevenheung.ca@gmail.com>
2021-10-02 13:11:27 -0600

    GET implemented

--- httpclient.py
+++ httpclient.py
@@ -21,6 +21,7 @@
 import sys
 import socket
 import re
+from typing import Tuple
 # you may use urllib to encode data appropriately
 import urllib.parse
 
@@ -33,48 +34,108 @@ class HTTPResponse(object):
         self.body = body
 
 class HTTPClient(object):
-    #def get_host_port(self,url):
+
+    def __init__(self):
+        super().__init__()
+        self.socket = None
 
     def connect(self, host, port):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((host, port))
-        return None
 
-    def get_code(self, data):
-        return None
+    def parse_url(self, url) -> Tuple[int, str, str]:
+        # first, split the HTTP/HTTPS header and get the path
+        print(url)
+        specifiedPort = url.split(":")[-1].split("/")[0]
+        path = url.split("//")[-1]
+        httpType = url.split("//")[0].strip()
+        try:
+            port = int(specifiedPort)
+        except ValueError:
+            if httpType == "https:":
+                port = 443
+            else:
+                port = 80
 
-    def get_headers(self,data):
-        return None
+        # next, get the host and path
+        items = [x for x in path.split("/") if x] # remove empty items
+        host = items[0].split(":")[0] # host is the first item in list, and remove potential specificed port ':'
+        items.pop(0)
 
-    def get_body(self, data):
-        return None
-    
+        if len(items) > 0:
+            path = '/'.join(items)
+        else:
+            path = '/'
+
+        print("Port:", port)
+        print("Host:", host)
+        print("Path:", path)
+        return (port, host, path)
+
+    def get_code(self, result) -> int:
+        code = result.split()[1]
+        
+        try:
+            code = int(code)
+        except ValueError:
+            code = 500
+
+        return code
+
+
+    def get_body(self, result) -> str:
+        body = result.split("\r\n")[-1]
+        return body
+
+
     def sendall(self, data):
-        self.socket.sendall(data.encode('utf-8'))
+        if self.socket:
+            self.socket.sendall(data.encode('utf-8'))
+        else:
+            print("Socket not initialized")
         
     def close(self):
-        self.socket.close()
+        if self.socket:
+            self.socket.close()
 
     # read everything from the socket
     def recvall(self, sock):
         buffer = bytearray()
         done = False
+        count = 1
         while not done:
+            print("recovering data:", count)
             part = sock.recv(1024)
             if (part):
                 buffer.extend(part)
             else:
                 done = not part
+            count += 1
         return buffer.decode('utf-8')
 
     def GET(self, url, args=None):
-        code = 500
-        body = ""
+        port, host, path = self.parse_url(url)
+
+        try:
+            self.connect(host, port)
+        except:
+            print("Invalid Host or Port!")
+
+        data = f"GET {path} HTTP/1.1\r\nHost: {host}\r\n\r\n"
+
+        self.sendall(data)
+
+        result = self.recvall(self.socket)
+        code = self.get_code(result)
+        body = self.get_body(result)
+
+        print("Code:", code)
+        print("Body:", body)
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
         code = 500
-        body = ""
+        body = ''
         return HTTPResponse(code, body)
 
     def command(self, url, command="GET", args=None):
@@ -85,7 +146,6 @@ class HTTPClient(object):
     
 if __name__ == "__main__":
     client = HTTPClient()
-    command = "GET"
     if (len(sys.argv) <= 1):
         help()
         sys.exit(1)
