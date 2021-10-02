0c937d4b6a69b7c8e8cdb58245176096397785f5
flyrobot27 <stevenheung.ca@gmail.com>
2021-10-02 14:46:29 -0600

    POST Complete and tested

--- httpclient.py
+++ httpclient.py
@@ -25,6 +25,8 @@ from typing import Tuple
 # you may use urllib to encode data appropriately
 import urllib.parse
 
+DEBUG = True
+
 def help():
     print("httpclient.py [GET/POST] [URL]\n")
 
@@ -33,6 +35,12 @@ class HTTPResponse(object):
         self.code = code
         self.body = body
 
+    def __str__(self) -> str:
+        return f"Code: {self.code}\r\nBody:\r\n{self.body}"
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
 class HTTPClient(object):
 
     def __init__(self):
@@ -40,22 +48,21 @@ class HTTPClient(object):
         self.socket = None
 
     def connect(self, host, port):
+        '''Connect to the host:port and set the socket object'''
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((host, port))
 
     def parse_url(self, url) -> Tuple[int, str, str]:
+        '''Parse the URL and return port, host and path'''
         # first, split the HTTP/HTTPS header and get the path
-        print(url)
         specifiedPort = url.split(":")[-1].split("/")[0]
         path = url.split("//")[-1]
         httpType = url.split("//")[0].strip()
         try:
             port = int(specifiedPort)
         except ValueError:
-            if httpType == "https:":
-                port = 443
-            else:
-                port = 80
+            # We do not handle https
+            port = 80
 
         # next, get the host and path
         items = [x for x in path.split("/") if x] # remove empty items
@@ -63,48 +70,73 @@ class HTTPClient(object):
         items.pop(0)
 
         if len(items) > 0:
-            path = '/'.join(items)
+            path = '/' + '/'.join(items)
         else:
             path = '/'
 
-        print("Port:", port)
-        print("Host:", host)
-        print("Path:", path)
+        if DEBUG:
+            print("Port:", port)
+            print("Host:", host)
+            print("Path:", path)
         return (port, host, path)
 
-    def get_code(self, result) -> int:
-        code = result.split()[1]
+    '''
+    def parse_url(self, url) -> Tuple[int, str, str]:
+        result = urllib.parse.urlparse(url)
+        port = result.port
+        host = result.netloc.split(':')[0]
+        path = result.path
+        if not path:
+            path = '/'
         
+        if DEBUG:
+            print("Port:", port)
+            print("Host:", host)
+            print("Path:", path)
+
+        return (port, host, path)
+    '''
+
+    def get_code(self, result) -> int:
+        '''Parse the HTTP response code'''
         try:
+            code = result.split()[1]
             code = int(code)
-        except ValueError:
+        except (ValueError, IndexError):
             code = 500
 
         return code
 
 
     def get_body(self, result) -> str:
+        '''Parse and get the response body'''
         body = result.split("\r\n")[-1]
         return body
 
 
     def sendall(self, data):
+        '''Send the data with the given socket. '''
         if self.socket:
             self.socket.sendall(data.encode('utf-8'))
         else:
             print("Socket not initialized")
         
     def close(self):
+        '''Close the socket'''
         if self.socket:
             self.socket.close()
 
-    # read everything from the socket
+    
     def recvall(self, sock):
+        '''Read everything from the socket'''
+        if not sock:
+            print("Socket must be set!")
+            return
+
         buffer = bytearray()
         done = False
         count = 1
         while not done:
-            print("recovering data:", count)
             part = sock.recv(1024)
             if (part):
                 buffer.extend(part)
@@ -114,14 +146,29 @@ class HTTPClient(object):
         return buffer.decode('utf-8')
 
     def GET(self, url, args=None):
+        '''Performs HTTP/1.1 GET'''
         port, host, path = self.parse_url(url)
 
         try:
             self.connect(host, port)
         except:
             print("Invalid Host or Port!")
+            return None
 
-        data = f"GET {path} HTTP/1.1\r\nHost: {host}\r\n\r\n"
+        # Detect and insert query string. Only accept Dictionaries. 
+        if args:
+            if type(args) == dict:
+                args = dict(args)
+                queryString = ""
+                for key, value in args.items():
+                    temp = f"{key}={value}&"
+                    queryString += temp
+                path += "?" + queryString
+
+        data = f"GET {path} HTTP/1.1\r\nHost: {host}\r\nAccept: */*\r\n\r\n"
+
+        if DEBUG:
+            print("GET request:", data)
 
         self.sendall(data)
 
@@ -129,13 +176,49 @@ class HTTPClient(object):
         code = self.get_code(result)
         body = self.get_body(result)
 
-        print("Code:", code)
-        print("Body:", body)
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
-        code = 500
-        body = ''
+        '''Performs HTTP/1.1 POST'''
+        port, host, path = self.parse_url(url)
+
+        try:
+            self.connect(host, port)
+        except:
+            print("Invalid Host or Port!")
+            return 
+
+        data = f"POST {path} HTTP/1.1\r\nHost: {host}\r\n"
+
+        if args: # check if args is supplied
+            if type(args) == dict:
+                args = dict(args)
+                data += "Content-Type: application/x-www-form-urlencoded\r\n"
+                body = ""
+                for key, value in args.items():
+                    temp = f"{key}={value}&"
+                    body += temp
+                body = body[:-1] # Remove the last &
+
+                contentLength = len(body)
+                data += f"Content-Length: {contentLength}\r\n\r\n"
+                data += body
+            else:
+                data += "Content-Type: text/plain\r\n"
+                data += f"Content-Length: {len(args)}\r\n"
+                data += args
+        else:
+            # Set content type to plain test and 0 content length
+            data += "Content-Type: text/plain\r\nContent-Length: 0\r\n\r\n"
+
+        if DEBUG:
+            print("POST request:", data)
+
+        self.sendall(data)
+
+        result = self.recvall(self.socket)
+        code = self.get_code(result)
+        body = self.get_body(result)
         return HTTPResponse(code, body)
 
     def command(self, url, command="GET", args=None):
@@ -146,6 +229,7 @@ class HTTPClient(object):
     
 if __name__ == "__main__":
     client = HTTPClient()
+    client.GET("")
     if (len(sys.argv) <= 1):
         help()
         sys.exit(1)
